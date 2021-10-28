56489655d8b9101a3d22afd7fd838c3c03a2ccc7
Garrett Saxton <garrett.saxton@salesforce.com>
2021-10-27 21:16:16 -0600

    pg 131

---
+++ internal/agent/agent.go
@@ -0,0 +1,75 @@
+package agent
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/wgsaxton/distlog/internal/discovery"
+	"github.com/wgsaxton/distlog/internal/log"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+type Agent struct {
+	Config
+
+	log        *log.Log
+	server     *grpc.Server
+	membership *discovery.Membership
+	replicator *log.Replicator
+
+	shutdown     bool
+	shutdowns    chan struct{}
+	shutdownLock sync.Mutex
+}
+
+type Config struct {
+	ServerTLSConfig *tls.Config
+	PeerTLSConfig   *tls.Config
+	DataDir         string
+	BindAddr        string
+	RPCPort         int
+	NodeName        string
+	StartJoinAddrs  []string
+	ACLModelFile    string
+	ACLPolicyFile   string
+}
+
+func (c Config) RPCAddr() (string, error) {
+	host, _, err := net.SplitHostPort(c.BindAddr)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s:%d", host, c.RPCPort), nil
+}
+
+func New(config Config) (*Agent, error) {
+	a := &Agent{
+		Config:    config,
+		shutdowns: make(chan struct{}),
+	}
+	setup := []func() error{
+		a.setupLogger,
+		a.setupLog,
+		a.setupServer,
+		a.setupMembership,
+	}
+	for _, fn := range setup {
+		if err := fn(); err != nil {
+			return nil, err
+		}
+	}
+	return a, nil
+}
+
+func (a *Agent) setupLogger() error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return err
+	}
+	zap.ReplaceGlobals(logger)
+	return nil
+}
+
